18a798972bf435d872287416dcd72bb0b57b6585
Adam Onishi <onishiweb@gmail.com>
2015-08-29 10:03:30 +0100

    Load critical CSS in head and use loadCSS to load remaining

--- app/routes/core-routes.js
+++ app/routes/core-routes.js
@@ -1,24 +1,35 @@
 var React = require('react/addons'),
-ReactApp = React.createFactory(require('../components/ReactApp'));
+	fs = require('fs'),
+	ReactApp = React.createFactory(require('../components/ReactApp')),
+	critical = '';
+
+fs.readFile('public/critical.css', 'utf8', function (err,data) {
+  if (err) {
+    console.log(err);
+  }
+  
+  critical = data;
+
+});
 
 module.exports = function(app) {
 
-	app.get('/', function(req, res){
-		// React.renderToString takes your component
-	    // and generates the markup
-		var reactHtml = React.renderToString(ReactApp({}));
-	    // Output html rendered by react
-		// console.log(myAppHtml);
-	    res.render('index.ejs', {reactOutput: reactHtml});
-	});
+  	app.get('/', function(req, res){
+  		// React.renderToString takes your component
+  	    // and generates the markup
+  		var reactHtml = React.renderToString(ReactApp({}));
+  	    // Output html rendered by react
+  		// console.log(myAppHtml);
+  	    res.render('index.ejs', {reactOutput: reactHtml, criticalCSS: critical });
+  	});
 
-	app.get('/index.html', function(req, res){
-		// React.renderToString takes your component
-	    // and generates the markup
-		var reactHtml = React.renderToString(ReactApp({}));
-	    // Output html rendered by react
-		// console.log(myAppHtml);
-	    res.render('index.ejs', {reactOutput: reactHtml});
-	});
+  	app.get('/index.html', function(req, res){
+  		// React.renderToString takes your component
+  	    // and generates the markup
+  		var reactHtml = React.renderToString(ReactApp({}));
+  	    // Output html rendered by react
+  		// console.log(myAppHtml);
+  	    res.render('index.ejs', {reactOutput: reactHtml, criticalCSS: critical });
+  	});
 
-};
+  };
